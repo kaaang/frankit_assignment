781607ba667e620ce21d108beea034e3a8b12abd
kaaang <kaaang@kakao.com>
2025-02-22 02:28:07 +0900

    Initial commit

---
+++ src/main/java/kr/co/frankit_assignment/FrankitAssignmentApplication.java
@@ -0,0 +1,13 @@
+package kr.co.frankit_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankitAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrankitAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/frankit_assignment/FrankitAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.frankit_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankitAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
