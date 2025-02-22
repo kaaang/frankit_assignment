577b9cb7caec30eeb89d7eec4e9fd978d5bd85ef
kaaang <kaaang@kakao.com>
2025-02-22 22:56:33 +0900

    :sparkles: 3: 상품 도메인 구성

---
+++ src/main/java/kr/co/frankit_assignment/core/product/Product.java
@@ -0,0 +1,30 @@
+package kr.co.frankit_assignment.core.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import kr.co.frankit_assignment.core.kernel.domain.BaseEntityAggregateRoot;
+import lombok.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PACKAGE)
+@Getter
+@Entity
+@Table(name = "product_bcs_products")
+public class Product extends BaseEntityAggregateRoot<Product> {
+    @Id private UUID id;
+
+    @Column private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column private int price;
+
+    @Column private int shippingFee;
+
+    @Column private UUID createdBy;
+}
